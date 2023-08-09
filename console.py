d615a35926380c3c1ca77a47501684c88bf7b43e
cindyjeby <cindyjeby@gmail.com>
2023-08-09 11:01:37 +0300

    create

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 """Defines the HBnb console"""
 
 import cmd
+import models
+
 """entry point for hbnb console"""
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """does nothing when an empythline is entered"""
         pass
+
+    def do_create(self, clsname=None):
+        """Creates an new instance of the BaseModel
+        saves the instance and print the id of the instance"""
+        if not clsname:
+            print('**class name missing **')
+        elif not self.clslist.get(clsname):
+            print('** class doesn\'t exist **')
+        else:
+            obj = self.clslist[clsname]()
+            models.storage.save()
+            print(obj.id)
 
     def do_quit(self, arg):
         """quit command to exit the program"""
