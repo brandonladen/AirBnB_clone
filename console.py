7c9ddf0120e843162f213c78fc2ae0f18b9e3f78
cindyjeby <cindyjeby@gmail.com>
2023-08-09 15:59:34 +0300

    destroy

--- console.py
+++ console.py
@@ -5,6 +5,8 @@
 import cmd
 import models
 from models.base_model import BaseModel
+from datetime import datetime
+from shlex import shlex
 
 """entry point for hbnb console"""
 
@@ -85,6 +87,43 @@ class HBNBCommand(cmd.Cmd):
                 return False
             print([str(s) for i, s in models.storage.all().items()
                    if type(s) is self.classlist.get(arg)])
+            
+    def do_update(self, arg):
+        """Updates an instance bsed on the classname and id"""
+        classname, objid, attrname, attrval = None, None, None, None
+        updatetime = datetime.now()
+        args = arg.split(' ', 3)
+        if len(args) > 0:
+            classname = args[0]
+        if len(args) > 1:
+            objid = args[1]
+        if len(args) > 2:
+            attrname = args[2]
+            if len(args) > 3:
+                attrval = list(shlex(arg[3]))[0].strip('"')
+            if not classname:
+                print('** class name missing **')
+            elif not objid:
+                print('** instance id missing **')
+            elif not attrname:
+                print('** attribute name missing **')
+            elif not attrval:
+                print('** value missing **')
+            elif not self.classlist.get(classname):
+                print('**class doen\'t exist')
+            else:
+                i = classname + "." + objid
+                obj = models.storage.all().get(i)
+                if not obj:
+                    print('** no instance found **')
+                else:
+                    if hasattr(obj, attrname):
+                        attrval = type(getattr(obj, attrname))(attrval)
+                    else:
+                        attrval = self.getType(attrval)(attrval)
+                        setattr(obj, attrname, attrval)
+                        obj.update_at = updatetime
+                        models.storage.save()
 
     def do_quit(self, arg):
         """quit command to exit the program"""
