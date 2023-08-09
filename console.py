3f466e3029f6798eda174b78016dbee39e97eb2f
cindyjeby <cindyjeby@gmail.com>
2023-08-09 11:19:09 +0300

    create console

--- console.py
+++ console.py
@@ -4,26 +4,28 @@
 
 import cmd
 import models
+from models.base_model import BaseModel
 
 """entry point for hbnb console"""
 
 class HBNBCommand(cmd.Cmd):
     """creates a custom command line"""
     prompt = '(hbnb) '
+    classlist = {'BaseModel': BaseModel}
 
     def emptyline(self):
         """does nothing when an empythline is entered"""
         pass
 
-    def do_create(self, clsname=None):
+    def do_create(self, classname=None):
         """Creates an new instance of the BaseModel
         saves the instance and print the id of the instance"""
-        if not clsname:
+        if not classname:
             print('**class name missing **')
-        elif not self.clslist.get(clsname):
+        elif not self.classlist.get(classname):
             print('** class doesn\'t exist **')
         else:
-            obj = self.clslist[clsname]()
+            obj = self.classlist[classname]()
             models.storage.save()
             print(obj.id)
 
