082a1c1e6a4fddf02e68c90e690796e4f1ac6ef4
brandonladen <brandonladen486@gmail.com>
2023-08-10 10:39:38 +0300

    modified models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,11 @@
 from models.engine.file_storage import FileStorage
 from .base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 storage = FileStorage()
 storage.reload()
